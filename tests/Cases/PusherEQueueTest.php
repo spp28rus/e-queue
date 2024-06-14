69331dca5b7b81103b2ce018d82a5158bd8c0a27
spp28 <sppetrovich@mail.ru>
2024-06-15 00:43:37 +0200

    created test of pusher

---
+++ tests/Cases/PusherEQueueTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Cases;
+
+use EQueue\EQueueWorker;
+use Tests\BaseEQueueTestCase;
+use Tests\Implementations\EQueueTestJob;
+use Tests\Implementations\EQueueTestStorage;
+use Tests\Implementations\EQueueTestWorkerManager;
+
+class PusherEQueueTest extends BaseEQueueTestCase
+{
+    public function test(): void
+    {
+        $storage = new EQueueTestStorage();
+
+        $job = new EQueueTestJob(
+            uuid: uniqid(),
+            entityId: uniqid(),
+            isException: false,
+        );
+
+        $storage->pushJob($job);
+
+        $this->assertTrue(
+            $storage->hasEntity($job->getEntityId())
+        );
+    }
+}
